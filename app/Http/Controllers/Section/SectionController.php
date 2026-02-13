9d66ebc197893195f693ba3c4dbf440b6f8b2b29
Martina <martinaemail@gmail.com>
2026-02-13 02:00:20 +0200

    refactor sectionController

--- app/Http/Controllers/Section/SectionController.php
+++ app/Http/Controllers/Section/SectionController.php
@@ -34,14 +34,74 @@ class SectionController extends Controller
             ]
         ], 201);
     }
+       
+    public function index() 
+    {
+        $sections = Section::all();
 
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Sections retrieved successfully',
+            'data' => [
+                'sections' => $sections,
+            ]
+        ], 200);
+    }
+    public function show(Section $section)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Section retrieved successfully',
+            'data' => [
+                'section' => [
+                    'name' => $section->name,
+                    'course_id' => $section->course_id,
+                ],
+            ]
+        ], 200);
+    }
 
+    public function update(StoreSectionRequest $request, Section $section)
+    {
+        $name = $request->input('name');
+        $courseId = $request->input('courseId');
 
+        $section->update([
+            "name" => $name,
+            "course_id" => $courseId,
+        ]);
 
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Section updated successfully',
+            'data' => [
+                'section' => [
+                    'name' => $name,
+                    'course_id' => $courseId,
+                ],
+            ]
+        ], 201);
+    }
 
+    public function destroy(Section $section)
+    {
+        $section->delete();
 
-
-
-
-
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Section deleted successfully',
+            'data' => null
+        ], 201);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('/section-store', [SectionController::class , 'store']);
+Route::get('/section-index', [SectionController::class , 'index']);
+Route::get('/section-show/{section}', [SectionController::class , 'show']);
+Route::put('/section-update/{section}', [SectionController::class , 'update']);
+Route::delete('/section-destroy/{section}', [SectionController::class , 'destroy']);
 
 
-
-Route::get('/section-store', [SectionController::class , 'store']);
-Route::get('/section-update', [SectionController::class , 'udate']);
 
 
 
