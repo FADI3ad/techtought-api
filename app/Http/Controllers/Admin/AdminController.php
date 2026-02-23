00c01a18b53c8dc4f778d5e493a2c10eb0803a8e
Foda <fadyemad687@gmail.com>
2026-02-23 15:00:13 +0200

    feat(admin): implement login functionality in AdminAuthController and add AdminSeeder for initial admin setup

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,41 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\AdminLoginRequest;
+
 use App\Models\Admin;
-use Illuminate\Support\Facades\Hash;
+
 
 class AdminController extends Controller
 {
-    public function login(AdminLoginRequest $request)
-    {
-        $email = $request->input('email');
 
-        $password = $request->input('password');
 
-        $admin = Admin::where('email', '=', $email)->first();
-
-        if (!$admin || !Hash::check($password, $admin->password)) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Invalid credentials'
-            ], 401);
-        }
-
-        $token = $admin->createToken('admin-dashboard-token')->plainTextToken;
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Login successful',
-            'data' => [
-                'admin' => [
-                    'id' => $admin->id,
-                    'name' => $admin->name
-                ],
-                'token' => $token
-            ]
-        ], 200);
-
-    }
 
 }
